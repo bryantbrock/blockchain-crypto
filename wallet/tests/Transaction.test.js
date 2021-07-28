0b0d929bdb1c917c4765aa717215c647bdee10e3
Bryant Brock <bryant.brock@bryant-brock.local>
2021-07-27 21:31:09 -0700

    complete transaction test and class

--- wallet/tests/Transaction.test.js
+++ wallet/tests/Transaction.test.js
@@ -1,4 +1,5 @@
 const Transaction = require('../Transaction');
+const { verifySignature } = require('../utils/config');
 const Wallet = require('../Wallet');
 
 describe('Transaction', () => {
@@ -31,6 +32,34 @@ describe('Transaction', () => {
     it('outputs the remaining value for the `senderWallet`', () => {
       expect(transaction.outputMap[senderWallet.publicKey])
         .toEqual(senderWallet.balance - amount)
+    })
+  })
+
+  describe('input', () => {
+    it('has an `input`', () => {
+      expect(transaction).toHaveProperty('input')
+    })
+
+    it('has a `timestamp` in the input', () => {
+      expect(transaction.input).toHaveProperty('timestamp')
+    })
+
+    it('it sets the `amount` to the `senderWallet` balance', () => {
+      expect(transaction.input.amount).toEqual(senderWallet.balance)
+    })
+
+    it('it sets the `address` to the `senderWallet` publicKey', () => {
+      expect(transaction.input.address).toEqual(senderWallet.publicKey)
+    })
+
+    it('signs the input', () => {
+      expect(
+        verifySignature({
+          publicKey: senderWallet.publicKey,
+          data: transaction.outputMap,
+          signature: transaction.input.signature,
+        })
+      ).toBe(true)
     })
   })
 })
